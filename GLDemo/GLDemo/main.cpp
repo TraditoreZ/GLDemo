8ded3758e5b21621813e1e5884d12649e71d29fe
zhaoenze <zhaoenze@wangyuan.com>
2021-10-15 16:33:14 +0800

    draw

--- GLDemo/GLDemo/main.cpp
+++ GLDemo/GLDemo/main.cpp
@@ -1,12 +1,27 @@
 #include "iostream"
-#include <GLFW\glfw3.h>
+#include <GLFW/glfw3.h>
 #include <glad/glad.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* windows);
 
-const unsigned int DEFAULT_SCR_WIDTH = 1024;
-const unsigned int DEFAULT_SCR_HEITHT = 768;
+const unsigned int DEFAULT_SCR_WIDTH = 800;
+const unsigned int DEFAULT_SCR_HEITHT = 600;
+
+
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char *fragmentShaderSource="#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
 
 int main()
 {
@@ -33,19 +48,97 @@ int main()
 	}
 	glViewport(0, 0, DEFAULT_SCR_WIDTH, DEFAULT_SCR_HEITHT);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	float vertices[] = {
+	-0.5f,-0.5f,0.0f,
+	0.5f,-0.5f,0.0f,
+	0.0f,0.5f,0.0f
+	};
+
+	// 创建一个定点缓冲对象
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// 创建一个着色器对象
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	// 将着色器源码附加到着色器对象
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	// 编译着色器
+	glCompileShader(vertexShader);
+
+	// 判断shader是否编译成功
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX:: COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// 着色器程序对象   将多个着色器合并之后链接完成的版本
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX:: COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	// 使用着色器
+	glUseProgram(shaderProgram);
+	// 不再需要着色器对象 直接删除掉
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	// ..:: 初始化代码（只运行一次 (除非你的物体频繁改变)） :: ..
+	// 1. 绑定VAO
+	glBindVertexArray(VAO);
+	// 2. 把顶点数组复制到缓冲中供OpenGL使用
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 3. 设置顶点属性指针
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		// 输入
 		processInput(window);
 
-		// 渲染
-		// ...
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
+		// 渲染
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// ...
 		// 检查事件并交换缓冲
-		glfwPollEvents();
 		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 	glfwTerminate();
 	return 0;
